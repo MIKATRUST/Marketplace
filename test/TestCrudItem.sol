86bc01effabff90d7bbc8af4b6ade0e3e800c699
Mikatrust <mikael.kais@gmail.com>
2018-08-25 15:54:43 +0200

    cleaning

--- test/TestCrudItem.sol
+++ test/TestCrudItem.sol
@@ -11,15 +11,16 @@ contract TestCrudItem is CrudItem{
 
   CrudItem cruditem = CrudItem(DeployedAddresses.CrudItem());
   uint index;
-
-/*
-  uint itemSku,
-  bytes32 itemName,
-  uint itemQuantity,
-  bytes32 itemDescription,
-  uint itemPrice,
-  bytes32 itemImage)
-*/
+  
+  /*
+  struct ItemStruct {
+    bytes32 itemName;
+    uint itemQuantity;
+    bytes32 itemDescription;
+    uint itemPrice;
+    bytes32 itemImage;
+    uint index;
+  }*/
 
   function testInsertCrudItem() public {
     //function insertCrudItem(uint itemSku, bytes32 itemName, uint itemQuantity,bytes32 itemDescription, uint itemPrice,bytes32 itemImage)
@@ -28,7 +29,6 @@ contract TestCrudItem is CrudItem{
     insertCrudItem(111113,"item 3",3,"desc 3",13,"images/BIKE3.jpeg");
     insertCrudItem(111114,"item 4",4,"desc 4",14,"images/BIKE4.jpeg");
     insertCrudItem(111115,"item 5",5,"desc 5",15,"images/BIKE5.jpeg");
-    //try insert already existing store, tbd with js
     Assert.equal(uint256(5), getCrudItemCount(), "5 items inserted correctly.");
   }
 
@@ -44,7 +44,7 @@ contract TestCrudItem is CrudItem{
     bytes32 expectedCrudItemDescription = "desc 2";
     uint expectedCrudItemPrice = 12;
     bytes32 expectedCrudItemImage = "images/BIKE2.jpeg";
-    //try request not a store, tbd with js
+
     (recCrudItemName,recCrudItemQuantity,recCrudItemDescription,recCrudItemPrice,recCrudItemImage,index) = getCrudItem(111112);
 
     Assert.equal(bytes32(recCrudItemName), bytes32(expectedCrudItemName), "recovered and expected itemName should match");
@@ -53,8 +53,6 @@ contract TestCrudItem is CrudItem{
     Assert.equal(bytes32(recCrudItemPrice), bytes32(expectedCrudItemPrice), "recovered and expected itemPrice should match");
     Assert.equal(bytes32(recCrudItemImage), bytes32(expectedCrudItemImage), "recovered and expected itemImage should match");
   }
-
-  //TBD : test function update
 
   function testGetCrudItemAtIndex() public {
     uint expectedCrudItemSku = 111112;
@@ -65,9 +63,7 @@ contract TestCrudItem is CrudItem{
   function testDeleteCrudItem() public {
     deleteCrudItem(111111);
     deleteCrudItem(111115);
-    // try delete not a store, tbd with js
     Assert.equal(uint256(3), getCrudItemCount(), "3 stores remaining.");
-
   }
 
   function testUpdateCrudItemQuantity() public {
@@ -87,6 +83,7 @@ contract TestCrudItem is CrudItem{
   }
 
   function testCrudItemCount() public {
+    Assert.equal(uint256(3), getCrudItemCount(), "3 store remaining.");
     deleteCrudItem(111112);
     deleteCrudItem(111113);
     deleteCrudItem(111114);
